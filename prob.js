fce3b7006229f50c86aedcb9f5cec5223f369ed4
ahadhasanjion <niallhoran3176@gmail.com>
2022-10-07 03:29:17 +0600

    js

--- prob.js
+++ prob.js
@@ -3,24 +3,26 @@ function radianToDegree(radian) {
     if (typeof radian == 'number') {
         let radiantodegree = (radian * 180 / Math.PI);
         let degree = radiantodegree.toFixed(2);
-        return degree;
+        return parseFloat(degree);
     } else {
         return 'Please Enter Radian Number';
     }
 }
 console.log(radianToDegree(10));
+
 //PROBLEM NUMBER 1 END
 //PROBLEM NUMBER 2 START
 function isJavaScriptFile(File) {
     if (typeof File !== 'string') {
-        return 'Please Type Valid File'
-    } else if (File.endsWith('.js')) {
+        return 'Please Type Valid File Nime'
+    } else if (File.toLowerCase().endsWith('.js')) {
         return true;
     } else {
         return false;
     }
 }
-console.log(isJavaScriptFile('mylove.js'));
+console.log(isJavaScriptFile('my.js'));
+
 //PROBLEM NUMBER 2 END
 //PROBLEM NUMBER 3 START
 function oilPrice(dissel, petrol, octane) {
@@ -36,8 +38,7 @@ function oilPrice(dissel, petrol, octane) {
         let totaloilPrice = disselPrice + petrolPrice + octanePrice;
         return totaloilPrice;
     }
-}
-console.log(oilPrice(0, 0, 1));
+}console.log(oilPrice(1, 1, 1,));
 //PROBLEM NUMBER 3 END
 //PROBLEM NUMBER 4 START
 function publicBusFare(TotalPublic) {
@@ -54,6 +55,7 @@ function publicBusFare(TotalPublic) {
     }
 }
 console.log(publicBusFare(365));
+
 //PROBLEM NUMBER 4 END
 //PROBLEM NUMBER 5 START
 let FriendOne = {
